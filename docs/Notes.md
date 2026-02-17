ef3c1bbe6d45ce0c486dd4dc1c1572208c50de53
Giada De Martino <giada-de-martino@etu.umontpellier.fr>
2026-02-17 15:17:18 +0100

    test deploy on vector

---
+++ docs/Notes.md
@@ -0,0 +1,14 @@
+### Things I did : 
+
+* Selected a project : StoryBook
+ 
+* Participated in the community Discord
+
+* Found a bug and reported it :
+BUG url : https://github.com/storybookjs/storybook/issues/33856
+
+* Took the issue 
+ISSUE url : https://github.com/storybookjs/storybook/issues/33856 
+
+* Made a PR :
+PR url : https://github.com/storybookjs/storybook/pull/33858
--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// Simple form submit alert (can enhance later)
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you! Your message has been sent.');
+const sections = [
+    "sections/hero.html",
+    "sections/about.html",
+    "sections/skills.html",
+    "sections/parcours.html",
+    "sections/projects.html",
+    "sections/contact.html"
+];
+
+const content = document.getElementById("content");
+
+sections.forEach(section => {
+    fetch(section)
+        .then(response => response.text())
+        .then(data => {
+            content.innerHTML += data;
+        });
 });
